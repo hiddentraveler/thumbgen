4a297a5aaa359ea195e0159d9caaecb762dfcb39
hiddentraveler <github.cincholoipon@aleeas.com>
2025-07-28 23:22:22 +0530

    Thumbnail generation V1

---
+++ app/spinner.tsx
@@ -0,0 +1,5 @@
+export default function Spinner() {
+  return (
+    <div className="h-6 w-6 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin" />
+  );
+}
---
+++ app/thumbGen.tsx
@@ -0,0 +1,205 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import { removeBackground } from "@imgly/background-removal";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+
+const presets = {
+  style1: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "rgba(255, 255, 255, 1)",
+    opacity: 1,
+  },
+  style2: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "rgba(0, 0, 0, 1)",
+    opacity: 1,
+  },
+  style3: {
+    fontSize: 100,
+    fontWeight: "bold",
+    color: "rgba(255, 255, 255, 0.8)",
+    opacity: 0.8,
+  },
+};
+
+const ThumbnailCreator = () => {
+  const [selectedStyle, setSelectedStyle] = useState("style1");
+  const [loading, setLoading] = useState(false);
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [processedImageSrc, setProcessedImageSrc] = useState<string | null>(
+    null,
+  );
+  const [canvasReady, setCanvasReady] = useState(false);
+  const [text, setText] = useState("POV");
+  const [font, setFont] = useState("arial");
+
+  const setSelectedImage = async (file?: File) => {
+    if (file) {
+      setLoading(true);
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const src = e.target?.result as string;
+        setImageSrc(src);
+
+        const blob = await removeBackground(src);
+        const processedUrl = URL.createObjectURL(blob);
+        setProcessedImageSrc(processedUrl);
+        setCanvasReady(true);
+        setLoading(false);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  useEffect(() => {
+    if (canvasReady) {
+      drawCompositeImage();
+    }
+  }, [canvasReady]);
+
+  const drawCompositeImage = () => {
+    if (!canvasRef.current || !canvasReady || !imageSrc || !processedImageSrc)
+      return;
+
+    const canvas = canvasRef.current;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    const bgImg = new Image();
+
+    bgImg.onload = () => {
+      canvas.width = bgImg.width;
+      canvas.height = bgImg.height;
+
+      ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+
+      let preset = presets.style1;
+      switch (selectedStyle) {
+        case "style2":
+          preset = presets.style2;
+          break;
+        case "style3":
+          preset = presets.style3;
+          break;
+      }
+
+      ctx.save();
+
+      // Calculate font size to fill image 90% of the canvas
+      ctx.textAlign = "center";
+      ctx.textBaseline = "middle";
+
+      let fontSize = 100;
+      let selectFont = "arial";
+
+      ctx.font = `${preset.fontWeight} ${fontSize}px ${selectFont}`;
+      const textWidth = ctx.measureText(text).width;
+      const targetWidth = canvas.width * 0.9;
+
+      fontSize *= targetWidth / textWidth;
+      ctx.font = `${preset.fontWeight} ${fontSize}px ${selectFont}`;
+
+      ctx.fillStyle = preset.color;
+      ctx.globalAlpha = preset.opacity;
+
+      const x = canvas.width / 2;
+      const y = canvas.height / 2;
+
+      ctx.translate(x, y);
+      ctx.fillText(text, 0, 0);
+      ctx.restore();
+
+      const fgImg = new Image();
+      fgImg.onload = () => {
+        ctx.drawImage(fgImg, 0, 0, canvas.width, canvas.height);
+      };
+
+      fgImg.src = processedImageSrc;
+    };
+
+    bgImg.src = imageSrc;
+  };
+
+  const handleDownload = async () => {
+    if (canvasRef.current) {
+      const link = document.createElement("a");
+      link.download = "image.png";
+      link.href = canvasRef.current.toDataURL();
+      link.click();
+    }
+  };
+
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <div className="flex w-full max-w-2xl flex-col items-center gap-5 ">
+        <div className="my-4 flex w-full flex-col items-center gap-3">
+          <Card className="w-full">
+            {imageSrc ? (
+              <>
+                {loading ? (
+                  <>
+                    <div className="flex items-center justify-center">
+                      <div className="h-10 w-10 animate-spin rounded-full border-2 border-dashed border-gray-800"></div>
+                    </div>
+                  </>
+                ) : (
+                  <div className="flex items-center justify-center">
+                    <canvas
+                      ref={canvasRef}
+                      className="max-h-lg h-auto w-full max-w-lg rounded-lg"
+                    ></canvas>
+                  </div>
+                )}
+              </>
+            ) : (
+              <Input
+                id="picture"
+                type="file"
+                accept="image/*"
+                onChange={(e) => setSelectedImage(e.target.files?.[0])}
+              />
+            )}
+          </Card>
+        </div>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle>Edit</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col gap-1.5">
+                <Label htmlFor="text">Text</Label>
+                <Input
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  id="text"
+                  placeholder="Text in thumbnail"
+                />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-wrap justify-between gap-2">
+            <Button onClick={() => handleDownload()}>Download</Button>
+            <Button onClick={drawCompositeImage}>Update</Button>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default ThumbnailCreator;
